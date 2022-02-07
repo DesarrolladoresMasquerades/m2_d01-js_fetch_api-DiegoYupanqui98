c6827240c41ac3ebae49b52f5cd78dfd8d745496
Diego Yupanqui <diegoyupanquichavarry@gmail.com>
2022-02-07 17:57:01 +0100

    promise all applied

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("JS Loaded");
+/*console.log("JS Loaded");
 
 fetch("https://pokeapi.co/api/v2/pokemon")
     .then((response) => response.json())
@@ -32,4 +32,62 @@ function buildPokedex(pokeDex) {
 
     }
     );
+}*/
+
+console.log("JS Loaded");
+
+fetch("https://pokeapi.co/api/v2/pokemon")
+    .then((response) => response.json())
+    .then(buildPokedex)
+    .catch(console.log);
+
+
+
+function buildPokedex(pokeDex) {
+    const pokeNames = pokeDex.results.map(pokemon => pokemon.name)
+    const pokeLinks = pokeDex.results.map(pokemon=>pokemon.url)
+    //const pokePics = []
+
+     const fetchPicsPromises = pokeLinks.map(link=>fetch(link))
+     Promise.all(fetchPicsPromises)
+        .then((response) => addToDom(pokeNames, pokeLinks,response))
+        
+
+
+
+    pokeDex.results.forEach((pokemon) => {
+        const newH1 = document.createElement("h1");
+        newH1.innerText = pokemon.name;
+
+        const newLink = document.createElement("a");
+        newLink.innerText = "Link to Pokémon";
+        newLink.href = pokemon.url;
+
+        const newImg = document.createElement("img");
+        fetch(pokemon.url)
+            .then((response) => response.json())
+            .then((onePokemon) => {
+                newImg.src = onePokemon.sprites.front_shiny;
+                document.body.appendChild(newH1);
+                document.body.appendChild(newImg);
+                document.body.appendChild(newLink);
+            });
+
+    });
+}
+
+function addToDom(names, links, pokemons){
+
+    for(let i = 0; i < pokemons.lenght; i++){
+    const newH1 = document.createElement("h1");
+    newH1.innerText = names[i];
+    const newLink = document.createElement("a");
+    newLink.innerText = "Link to Pokémon";
+    newLink.href = links[i];
+    const newImg = document.createElement("img");
+    newImg.src = pokemons[i].sprites.front_shiny;
+    document.body.appendChild(newH1);
+    document.body.appendChild(newImg);
+    document.body.appendChild(newLink);
+}
 }
